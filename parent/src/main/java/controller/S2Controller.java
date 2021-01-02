b12edda999b012335f7bb1074bb99c44df02afea
tms20201018 <TanMingShi01@163.com>
2021-01-02 16:41:01 +0800

    修改s2 tms

--- parent/src/main/java/controller/S2Controller.java
+++ parent/src/main/java/controller/S2Controller.java
@@ -5,13 +5,6 @@ public class S2Controller {
     public void mmm(){
         System.out.println("12315644611");
         System.out.println("啥都看不了吗？？？？？邪门");
-
-        System.out.println("啥都看不了");
-
-
-
-
-
         System.out.println("hzb冲突添加");
     }
 }
