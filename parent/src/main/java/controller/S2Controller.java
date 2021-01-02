fb71a1d04c85bccc0638bd1e9bc8545cd2d2680c
tms20201018 <TanMingShi01@163.com>
2021-01-02 16:34:02 +0800

    修改s2controller第二次 tms

--- parent/src/main/java/controller/S2Controller.java
+++ parent/src/main/java/controller/S2Controller.java
@@ -4,6 +4,6 @@ public class S2Controller {
 
     public void mmm(){
         System.out.println("12315644611");
-        System.out.println("啥都看不了");
+        System.out.println("啥都看不了吗？？？？？邪门");
     }
 }
