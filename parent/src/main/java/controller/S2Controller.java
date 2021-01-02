6a6d71606589ffe9feb68026f615c723a17a8d82
tms20201018 <TanMingShi01@163.com>
2021-01-02 16:59:23 +0800

    基地

--- parent/src/main/java/controller/S2Controller.java
+++ parent/src/main/java/controller/S2Controller.java
@@ -4,7 +4,7 @@ public class S2Controller {
 
     public void mmm(){
         System.out.println("12315644611");
-        System.out.println("啥都看不了吗？？？？？邪门");
+        System.out.println("啥都看不了吗？？？？？邪门，再次邪门。。。。。");
         System.out.println("hzb冲突添加");
     }
 }
